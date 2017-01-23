81bfb80e6f7274dcbeff982c85487cbe1363b20f
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-01-23 17:43:45 -0600

    creates the SQL connection in connection.js

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+});
+
+
+module.exports = function(app) {
+
+    connection.connect(function(err) {
+        if (err) throw err;
+
+        connection.query("SELECT * FROM burgers", function(err, res) {
+
+        });
+    });
+};

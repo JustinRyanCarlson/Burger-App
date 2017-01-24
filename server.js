045778c154bd09503e523e77c5731129399213cd
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-01-24 17:19:21 -0600

    adds routes variable to server.js, adds express-handlebars variables and engine to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var app = express();
+var exphbs = require("express-handlebars");
 var bodyParser = require('body-parser');
 require('./config/connection.js')(app);
 var PORT = process.env.PORT || 3000;
@@ -9,3 +10,12 @@ var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
     console.log('Listening on port: ' + PORT);
 });
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+var routes = require("./controllers/burgers_controller.js");
+app.use("/", routes);
